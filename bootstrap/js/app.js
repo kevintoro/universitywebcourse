b9ea3679342a0e51b598ee66b7d7906484ce772b
Kevin Toro <kevinandrestg7@gmail.com>
2020-09-29 13:04:56 -0500

    add jQuery

--- bootstrap/js/app.js
+++ bootstrap/js/app.js
@@ -1,3 +1,11 @@
 $("#selh3").click(function (event) {
-  $("#contenedor").load("../selecciones/colombia.html");
+  $("#contenedor").load("selecciones/colombia.html");
+  $("#navbarToggler ul li").removeClass("active");
+  $("#dropMenuGroupH").addClass("active");
+});
+
+$("#navInicio").click(function (event) {
+  $("#navbarToggler ul li").removeClass("active");
+  $("#navbarToggler ul li a").removeClass("active");
+  $("#navInicio").addClass("active");
 });
